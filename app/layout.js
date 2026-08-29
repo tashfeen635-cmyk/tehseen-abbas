a2dd71d7807df80447fd17a14f6c970cef2fa111
tashfeen635-cmyk <tashfeen635@gmail.com>
2026-08-30 02:00:10 +0500

    Add favicon using site logo

--- app/layout.js
+++ app/layout.js
@@ -17,6 +17,7 @@ export default function RootLayout({ children }) {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
         />
+        <link rel="icon" href="/logo/logo.png" type="image/png" />
       </head>
       <body>{children}</body>
     </html>
